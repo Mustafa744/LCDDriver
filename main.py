e9debc4bf0f66a1ffed75b1e2dc7d26469b24718
mustafa744 <mustafaashraf744@gmail.com>
2025-03-02 19:07:32 +0200

    //

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from gpio_handler import GPIOHandler
 from spi_handler import SPIHandler
 from const import ILI9340, Colors
 import time
+
 gpio = GPIOHandler()
 spi = SPIHandler()
 display = DisplayHandler(gpio_handler=gpio, spi_handler=spi, commands=ILI9340)
 
 if __name__ == "__main__":
     display.init_display()
-    time.sleep
+    time.sleep(0.1)
     display.reset_display()
+    time.sleep(0.1)
     display.fill_screen(Colors.RED)
